84619dd29f160d3618e4b95c4356dcc9a51fb7c8
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-04-14 14:01:53 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const dotenv = require('dotenv');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+dotenv.config();
+
+const app = express();
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({dest: 'images', fileFilter: fileFilter })
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use(upload.single('image'));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect( process.env.MONGODB_URI )
+  .then(result => {
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch(err => console.log(err));
